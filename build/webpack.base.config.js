c1fefe2648659e6069064e88b87ccdeac60da26a
steviehao <stevieforwork@126.com>
2020-03-04 21:59:07 +0800

    修改webpack配置，修复了无法在浏览器热更新的问题

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,50 +1,50 @@
-const path = require('path');
+const Path = require('path');
 const HappyPack = require('happypack'); // 开启多个子线程并发执行
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: ["babel-polyfill", path.resolve(__dirname, '../src/index.js')],
-    output: {
-        filename: "main.[hash:8].js",
-        path: path.resolve(__dirname, "../dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'], // 从右向左解析
-                exclude: /node_modules/
-            },
-            {
-                test: /\.jsx?$/,
-                use: {
-                    loader: 'happypack/loader?id=babel'
-                },
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.scss', '.json']
-    },
-    plugins: [
-        new HappyPack({
-            id: 'babel', // 与loader里的配置项对应
-            threads: 4, // 配置子进程数
-            loaders: ['babel-loader'] // 指定loader
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/template/index.html'),
-            filename: 'home.html'
-        }),
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: '../dist'
-        })
+  entry: ['babel-polyfill', Path.resolve(__dirname, '../src/index.js')],
+  output: {
+    filename: 'main.[hash:8].js',
+    path: Path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'], // 从右向左解析
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'happypack/loader?id=babel'
+        },
+        exclude: /node_modules/
+      }
     ]
-}
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.scss', '.json']
+  },
+  plugins: [
+    new HappyPack({
+      id: 'babel', // 与loader里的配置项对应
+      threads: 4, // 配置子进程数
+      loaders: ['babel-loader'] // 指定loader
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['./dist']
+    }),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/template/index.html'),
+      filename: 'index.html'
+    })
+  ]
+};
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -3,23 +3,23 @@ const base = require('./webpack.base.config');
 const WebpackParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 const production = {
-    mode: 'production',
-    plugins: [
-        new WebpackParallelUglifyPlugin({
-            uglifyJS: {
-                mangle: false, // 是否混淆代码
-                output: {
-                    beautify: false, // 代码压缩成一行 true为不压缩 false压缩
-                    comments: false // 是否去掉注释
-                },
-                compress: {
-                    drop_console: true, // 删除console
-                    collapse_vars: true, //  把定义一次的变量，直接使用，取消定义变量
-                    reduce_vars: true // 合并多次用到的值，定义成变量
-                }
-            }
-        })
-    ]
-}
+  mode: 'production',
+  plugins: [
+    new WebpackParallelUglifyPlugin({
+      uglifyJS: {
+        mangle: false, // 是否混淆代码
+        output: {
+          beautify: false, // 代码压缩成一行 true为不压缩 false压缩
+          comments: false // 是否去掉注释
+        },
+        compress: {
+          drop_console: true, // 删除console
+          collapse_vars: true, //  把定义一次的变量，直接使用，取消定义变量
+          reduce_vars: true // 合并多次用到的值，定义成变量
+        }
+      }
+    })
+  ]
+};
 
 module.exports = merge(base, production);
